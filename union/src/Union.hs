7af8ad58ea76834449517078ffe233485d989ea2
apalacioj <68928378+apalacioj@users.noreply.github.com>
2021-09-17 15:08:51 -0500

    Documentation update

--- union/src/Union.hs
+++ union/src/Union.hs
@@ -37,31 +37,38 @@ newacceptingstate map []=[]
 newacceptingstate   map (x:xs) = [map Map.! x]++ (newacceptingstate map xs)
             
 -- |This function receives the states of each automata and replace them with a list since 
--- 0 until the sum of the number of states of the two automata.
+-- 1 until the sum of the number of states of the two automata (the zero represents the new
+-- initial state).
 newstates:: [Natural]-> [Natural]->[Natural]
-newstates statesFA1 statesFA2 = [0 .. (fromIntegral (length statesFA1 + length statesFA2))]
+newstates statesFA1 statesFA2 = [0]++[1 .. (fromIntegral (length statesFA1 + length statesFA2))]
 
 -- |This function receives the transitions of the two automata and concatenates them is a list. 
 movementslist:: [Move Natural]-> [Move Natural]-> [Move Natural]
 movementslist movementsFA1 movementsFA2=  movementsFA1 ++ movementsFA2
 
--- |This function receives a list with the union automata states and a number that represents 
--- an iterator
+-- |This function receives a list with the union of the states of the two automata 
+-- and a number that represents an iterator, where the keys are going to be each 
+-- states and the values are going to be the iterator.
 statesmap:: [Natural]-> Natural -> Map Natural Natural
 statesmap [] cont = Map.empty 
 statesmap (x:xs) cont = Map.insert x cont (statesmap xs (cont+1))
 
--- |This function receives a list with th moves of the automata and a map 
+-- |This function receives a list with the transitions of the automata and a map that contains 
+-- the states of the union automata. The function implements recursively an other function 
+-- (movements2) to modify each movement with the states of the union automata. 
 movements1:: [Move Natural] -> Map Natural Natural -> [Move Natural]
 movements1 [] map =[]
 movements1 (x:xs) map=  [movements2 x map ] ++ (movements1 xs map )
 
--- |
+-- |This function receives a movement, a map and returns a movement. The function updates the states
+-- of each movement with the new states of the union automata. 
 movements2 :: Move Natural -> Map Natural Natural -> Move Natural
 movements2 (Move state1 char state2) map = Move (map Map.! state1) char (map Map.! state2)
 movements2 (Emove state1 state2) map = Emove (map Map.! state1)  (map Map.! state2)
 
--- |
+-- |The f function receives the states, transitions and the initial state of each automata and returns 
+-- a list with the transitions (including the epsilon transitions since the new initial state created 
+-- until each initial state of the two automata) of the union automata.
 f :: [Natural] -> [Move Natural] -> Natural -> [Natural] -> [Move Natural] -> Natural -> [Move Natural]
 f states1 moves1 num1 states2 moves2 num2 = (movements1 moves1 (statesmap states1 1))++(movements1 moves2 (statesmap states2 ((fromIntegral (length states1)+1))))++[Emove 0 (((statesmap states1 1) Map.! num1)), Emove 0 (((statesmap states2 ((fromIntegral (length states1)+1))) Map.! num2))]
 
